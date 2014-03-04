ba7ca59b47131b60f35b3de129e7b1cdb12692bd
foolball <macmac24@yahoo.com>
2014-03-03 17:12:19 -0800

    added start/end indicator when creating lines
    shows user where the start and end clicks are when creating a line. Also
    fixed clear canvas button bug.

--- README.md
+++ README.md
@@ -3,5 +3,9 @@ Canvas-Line-Samples
 
 Working samples of HTML5 Canvas Lines
 
+3-3-2014
+Added the a start and end indicator when creating the line.
+Also fixed a bug in "clear canvas" button. 
+
 2-28-2014
 added code to display the mouse position when user clicks the mouse.
--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@ var lastClick = [0, 0];
 
 var canvas = document.getElementById('myCanvas');
 
-// on mouse click, gets mouse position and displays it on canvas.
+// draws a line by getting starting and ending point based on mouse clicks.
 $(canvas).on('click', function (evt) {
     drawLine(canvas, evt);
 });
 
 // on mouse click, clear canvas
 $('#clear').on('click', function () {
+    //drawRectangle(canvas);
     clearCanvas(canvas);
 });
 
@@ -26,6 +27,8 @@ function writeMessage(canvas, message) {
 function clearCanvas(canvas) {
     var context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
+    // reset click counter
+    clicks = 0;
 }
 
 // function to get mouse position
@@ -43,21 +46,50 @@ function drawLine(canvas, evt) {
     var mousPos = getMousePos(canvas, evt);
     var context = canvas.getContext('2d');
 
-    if (clicks != 1)
+    if (clicks != 1) {
+        drawRectangle(canvas, mousPos, true);
         clicks++;
+    }
     else {
         context.beginPath();
         context.moveTo(lastClick[0], lastClick[1]);
         context.lineTo(mousPos.x, mousPos.y);
         context.stroke();
+        drawRectangle(canvas, mousPos, false);
         clicks = 0;
     }
 
-    lastClick = [mousPos.x, mousPos.y];
-   // lastClick[0] = mousPos.x;
-    //lastClick[1] = mousPos.y;
+    lastClick = [mousPos.x, mousPos.y]; // saves the last click
 
     //var message = 'Mouse position: ' + mousPos.x + ',' + mousPos.y;
     //writeMessage(canvas, message);
+}
+
+// draw small rectangle indicating whether it's the beginning or end of the line
+function drawRectangle(canvas, coordinates, isStart) {
+    var context = canvas.getContext('2d');
+
+    context.beginPath();
+    context.rect(coordinates.x + 5, coordinates.y + 5, 20, 10);
+    context.fillStyle = 'yellow';
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = 'black';
+    context.stroke();
+
+    addText(canvas, coordinates, isStart);
+
+}
+
+// adds 'S' for start or 'E' for end depending on when the user clicks to create a line.
+function addText(canvas, coordinates, isStart) {
+    var context = canvas.getContext('2d');
+
+    context.font = '10pt Calibri';
+    context.fillStyle = 'blue';
+    if (isStart) // if the user click is in the beginning...
+        context.fillText('S', coordinates.x + 13, coordinates.y + 14);
+    else
+        context.fillText('E', coordinates.x + 13, coordinates.y + 14);
 }
 
